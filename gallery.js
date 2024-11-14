a9ddec7a39716af78fd7855f1a2ab20b4e936cb3
Phoenix-Fire2077 <2015harrison@gmail.com>
2024-11-13 23:40:11 -0800

    V0.4.2 Final
    V0.4.2
    11/13/24 commit 0.4.2 - Gallery basic gallery functionality has been added!

--- gallery.js
+++ gallery.js
@@ -4,10 +4,13 @@ let minYear = 2024;
 
 window.addEventListener('load' , function() {
     loadActive(activeGalleryYear);
+    directionCheck(activeGalleryYear);
 
     // buttons (add more for the specific buttons)
     let button2025 = document.getElementById("button2025");
     let button2024 = document.getElementById("button2024");
+    let buttonleft = document.getElementById("buttonleft");
+    let buttonright = document.getElementById("buttonright");
 
     button2025.onclick = function() {
         loadActive(2025);
@@ -17,10 +20,18 @@ window.addEventListener('load' , function() {
         loadActive(2024);
     }
 
+    buttonleft.onclick = function() {
+        changeDirection(1);
+    }
+    buttonright.onclick = function() {
+        changeDirection(-1);
+    }
+
 
 });
 
 function loadActive(active) {
+    directionCheck(active);
     let exibith2 = document.getElementById("exibith2");
     let button;
     let gallery;
@@ -41,4 +52,27 @@ function loadActive(active) {
     button.className = "inactive";
     button.disabled = true;
     gallery.style.display = "flex";
+    activeGalleryYear = active;
+}
+
+function changeDirection(direction) {
+    loadActive(activeGalleryYear + direction);
+}
+
+function directionCheck(active) {
+    let buttonleft = document.getElementById("buttonleft");
+    let buttonright = document.getElementById("buttonright");
+
+    buttonleft.className = "active";
+    buttonleft.disabled = false;
+    buttonright.className = "active";
+    buttonright.disabled = false;
+
+    if (maxYear <= active) {
+        buttonleft.className = "inactive";
+        buttonleft.disabled = true;
+    } else {
+        buttonright.className = "inactive";
+        buttonright.disabled = true;
+    }
 }
